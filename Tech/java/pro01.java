9b37597b5d7ced336246a44f57906b2638baeba0
Mr. Akash Rathod <114861476+ak-akash03@users.noreply.github.com>
2023-01-01 22:15:23 +0530

    logic change....

--- Tech/java/pro01.java
+++ Tech/java/pro01.java
@@ -1,15 +1,11 @@
 public class pro01 {
     public static void main(String[] args) {
         System.out.println("hello aksah : ");
-
         int a = 0;
         int b = 0;
         System.out.println(a++);
         System.out.println(a--);
         System.out.println(b--);
         System.out.println(++b);
-
-
-
     }
 }
