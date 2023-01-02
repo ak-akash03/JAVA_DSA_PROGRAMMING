3c0cd97c823ac789df2c84b9d49b50ab76d5db07
Mr. Akash Rathod <114861476+ak-akash03@users.noreply.github.com>
2023-01-02 11:05:44 +0530

    Previous Update

--- Tech/java/pro01.java
+++ Tech/java/pro01.java
@@ -1,8 +1,7 @@
 public class pro01 {
     public static void main(String[] args) {
         System.out.println("hello aksah : ");
-        int a = 0;
-        int b = 0;
+        int a = 0, b = 0;
         System.out.println(a++);
         System.out.println(a--);
         System.out.println(b--);
