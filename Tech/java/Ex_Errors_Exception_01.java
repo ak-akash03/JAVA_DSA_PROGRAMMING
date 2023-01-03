014c27061ab58ac31e20073fa312628d7520631d
Mr. Akash Rathod <114861476+ak-akash03@users.noreply.github.com>
2023-01-03 18:38:14 +0530

    Remove comments

--- Tech/java/Ex_Errors_Exception_01.java
+++ Tech/java/Ex_Errors_Exception_01.java
@@ -5,23 +5,23 @@ public class Ex_Errors_Exception_01 {
 
           System.out.println("hello akash : ");
 
-//        int a = 1                           //   <==   Sytax Errors :
-//        System.out.println(a);
+        int a = 1                           //   <==   Sytax Errors :
+        System.out.println(a);
 
-//        Scanner sc = new Scanner(System.in);
-//        int a = sc.nextInt();
-//        int b = sc.nextInt();
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+       int b = sc.nextInt();
 
-//        System.out.println(a + b);           //  <==      Runtime Errors :
+        System.out.println(a + b);           //  <==      Runtime Errors :
 
 
         // Write a programs to print all prime numbers betweens 1 to 10 .
 
 
-//        System.out.println(2);
-//        for (int i = 1; i < 5; i++) {
-//            System.out.println(2*i+1);          //  <== Logical Errors :
-//        }
+        System.out.println(2);
+        for (int i = 1; i < 5; i++) {
+            System.out.println(2*i+1);          //  <== Logical Errors :
+        }
 
     }
 }
