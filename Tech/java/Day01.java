ec082211e5568fac9a25f61522fad6e680eee460
Mr. Akash Rathod <114861476+ak-akash03@users.noreply.github.com>
2023-01-19 12:07:26 +0530

    Code

--- Tech/java/Day01.java
+++ Tech/java/Day01.java
@@ -2,9 +2,9 @@
 // Basic Syntax of the java programms 
 public class Day01 {                                   // Class
     public static void main(String[] args) {        // Main Fuction :
-//        System.out.println("hello akash : ");
-//        System.out.println("hello world");            // Simple code :
-//        System.out.println("hello class");
-          System.out.println("hello class"); 
+        System.out.println("hello akash : ");    
+        System.out.println("hello world");            // Simple code :
+        System.out.println("hello class");
+        System.out.println("hello class"); 
     }
 }
