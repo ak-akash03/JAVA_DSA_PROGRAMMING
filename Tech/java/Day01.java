01e6cf3d9c14736ee5484365c2c7e26309f06d2a
Mr. Akash Rathod <114861476+ak-akash03@users.noreply.github.com>
2023-02-10 10:50:28 +0530

    Logic clear

--- Tech/java/Day01.java
+++ Tech/java/Day01.java
@@ -1,4 +1,4 @@
-// hello akash
+
 // Basic Syntax of the java programms 
 public class Day01 {                                   // Class
     public static void main(String[] args) {        // Main Fuction :
