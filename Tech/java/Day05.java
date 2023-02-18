27080ac5c3045f19d7ef8be1c7f1940825e7d88b
Mr. Akash Rathod <114861476+ak-akash03@users.noreply.github.com>
2023-02-18 16:15:29 +0530

    Logic add

--- Tech/java/Day05.java
+++ Tech/java/Day05.java
@@ -1,7 +1,6 @@
 public class Day05 {
     public static void main(String[] args) {
         System.out.println("hello akash : ");
-
         int a = 100;
         int b = 200;
         int c = a + b ;
