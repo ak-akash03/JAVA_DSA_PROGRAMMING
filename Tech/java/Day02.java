bdd38f3635bc9d270d3b8bd94ad0faf631935400
Mr. Akash Rathod <114861476+ak-akash03@users.noreply.github.com>
2023-02-25 01:52:15 +0530

    .........

--- Tech/java/Day02.java
+++ Tech/java/Day02.java
@@ -4,6 +4,7 @@ public class Day02 {
 
         int a = 10;             // int ==> is the data type         // a ==> is the varible     // 10 ==> is the value of a variable
         System.out.println(a);
+        
 
 
     }
